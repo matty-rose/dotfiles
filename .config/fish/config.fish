c745099fedb3be3eb381751bc6c0e1eaa30a05fe
Matthew Rose <matthew.rose@maxkelsen.com>
2020-12-02 18:43:43 +1000

    Change git stuff from alias to abbreviations, add exa abbreviations, add fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,25 @@
+# Abbreviations
+abbr -a e nvim
+abbr -a gst "git status"
+abbr -a gad "git add"
+abbr -a gch "git checkout"
+abbr -a gchb "git checkout -b"
+abbr -a gcm "git commit -m"
+abbr -a gcp "git cherry-pick"
+
+# Exa
+if command -v exa > /dev/null
+    abbr -a l 'exa'
+    abbr -a ls 'exa'
+    abbr -a ll 'exa -l'
+    abbr -a lll 'exa -la'
+    abbr -a la 'exa -la'
+else
+    abbr -a l 'ls'
+    abbr -a ll 'ls -l'
+    abbr -a lll 'ls -la'
+end
+
 # Fish Stuff
 set -g theme_display_docker_machine yes
 set -g theme_display_virtualenv yes
@@ -6,7 +28,6 @@ set -g theme_display_host ssh
 set -g theme_display_git_master_branch yes
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts no
-set -g theme_color_scheme solarized
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_use_abbreviated_branch_names no
 set -g theme_newline_cursor yes
@@ -21,16 +42,10 @@ eval /Users/matt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 set PATH /Users/matt/anaconda3/condabin:/Users/matt/anaconda3/bin $PATH
 
 # Double virtual env in prompt
-set -g VIRTUAL_ENV_DISABLE_PROMPT true
+set -gx VIRTUAL_ENV_DISABLE_PROMPT true
 
 # Aliases
-alias gst="git status"
-alias gad="git add"
-alias gch="git checkout"
-alias gchb="git checkout -b"
-alias gcm="git commit -m"
 alias config="/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME"
-alias gcp="git cherry-pick"
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/matt/google-cloud-sdk/path.fish.inc' ]; . '/Users/matt/google-cloud-sdk/path.fish.inc'; end
@@ -38,7 +53,20 @@ if [ -f '/Users/matt/google-cloud-sdk/path.fish.inc' ]; . '/Users/matt/google-cl
 #kubectl krew
 set -gx PATH $PATH $HOME/.krew/bin
 
+# Go
 set -gx PATH $PATH $HOME/go/bin
 
 # Set kubeeditor
 set -gx KUBE_EDITOR /usr/local/bin/nvim
+
+function fish_greeting
+	echo
+	echo -e (uname -n | awk '{print " \\\\e[1mUser: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -ne (\
+	    df -l -h | \
+	    grep -E 'dev/disk1s1' |
+	    awk '{print " \\\\e[1mDisk Usage:\\\\e[0m  "$4" Available\n  "$3"/"$2" Used\n  Capacity: "$5}'
+	)
+	echo
+	echo
+end
