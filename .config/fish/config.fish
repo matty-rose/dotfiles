9b43b9f07d8f32481543e1eb866c662bea6ecac4
Matthew Rose <matthew.rose@maxkelsen.com>
2021-01-10 16:01:57 +1000

    add vimdiff abbreviation, and coloured man page output

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,7 @@ abbr -a grc "git rebase --continue"
 abbr -a grs "git rebase --skip"
 abbr -a pa "source (poetry env info --path)/bin/activate.fish"
 abbr -a cpom "config push origin master"
+abbr -a vimdiff "nvim -d"
 
 # Exa
 if command -v exa > /dev/null
@@ -84,3 +85,12 @@ function fish_greeting
 	echo
 	echo
 end
+
+# Coloured man page
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
