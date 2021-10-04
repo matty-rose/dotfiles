2ab333e9108613227f771f9f752f57017cef2d3d
Matthew Rose <matthew.rose@maxkelsen.com>
2021-10-04 13:20:43 +1000

    working xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,20 @@
 import XMonad
 
-main = xmonad $ def
-    {
-    terminal = "alacritty"
+import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+import XMonad.Hooks.DynamicLog
+
+main :: IO ()
+main = xmonad 
+   =<< statusBar "xmobar" def toggleStrutsKey myConfig
+  where
+    toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+    toggleStrutsKey XConfig{ modMask = m } = (m, xK_b)
+
+myConfig = def
+    { terminal = "alacritty"
     }
+  `additionalKeysP`
+    [ ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("M-S-]", spawn "brave-browser"             )
+    ]
