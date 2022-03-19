9ec5d03a54c9a0f27a536496dd797cd77664256a
Matthew Rose <matthewrose153@gmail.com>
2022-03-19 17:15:05 +1000

    Multiple Updates
    - Adds different git user profiles/GPG keys for work vs personal
    - Adds Homebrew on Linux env vars to config
    - Uses new nvim diagnostic commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -151,6 +151,17 @@ fish_add_path $HOME/.yarn/bin
 # Protobuf Compiler
 fish_add_path $HOME/.protobuf/bin
 
+# Set brew on Linux vars
+switch (uname)
+    case Linux
+        set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+        set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+        set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+        set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+        set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+        set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+end
+
 #================================================
 #                     OTHER
 #================================================
---
+++ .config/git/config.mk
@@ -0,0 +1,4 @@
+[user]
+    name = Matthew Rose
+    email = matthew.rose@maxkelsen.com
+	signingkey = 9D28A344B3428369
