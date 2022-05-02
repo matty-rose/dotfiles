77900988b664c2f55e7b81db1910a03071e2698e
Matthew Rose <matthew.rose@maxkelsen.com>
2022-05-02 14:21:18 +1000

    update tab complete start

--- .config/nvim/lua/matt/nvim-cmp.lua
+++ .config/nvim/lua/matt/nvim-cmp.lua
@@ -32,29 +32,18 @@ local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
 -- Use (s-)tab to:
 --- move to prev/next item in completion menuone
 --- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
+  if vim.fn.pumvisible() then
     return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
   else
-    return vim.fn['compe#complete']()
+    return t "<Tab>"
   end
 end
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
+  if vim.fn.pumvisible() then
     return t "<C-p>"
   else
     return t "<S-Tab>"
