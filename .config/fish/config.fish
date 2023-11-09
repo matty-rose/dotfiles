8715ad958f9fefaaab4cd39cd2df4eda11244401
Matt Rose <mattyrose@canva.com>
2023-11-10 09:26:52 +1000

    formatting + nix update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 #                   Nix
 #================================================
 
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish' ]
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
 end
 
 #================================================
