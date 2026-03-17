a8fe6fa8287b440cc0dce764309b13baf7664912
Matt Rose <mattyrose@canva.com>
2026-03-17 12:09:04 +1000

    .

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+if vim.loader then
+  vim.loader.enable()
+end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
